5dd56bc94299ef05ef2efd1d9640447dd118cf1d
Auriol1601 <Auriol1601@github.com>
2024-12-17 18:42:39 +0100

    accueil

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+window.addEventListener("load", () => {
+    // Masquer le loader
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+  
+    // Afficher le contenu principal
+    const content = document.getElementById("content");
+    content.style.display = "block";
+  });
+  
